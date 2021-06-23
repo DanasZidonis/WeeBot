e1aa704632646b2ba4e0dfe116cbfc3810a4297f
DC_Danas <danas@dreamcubator.club>
2021-06-23 14:10:07 +0300

    Add project files.

---
+++ WeeBot/Program.cs
@@ -0,0 +1,86 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WeeBot
+{
+	public class Program
+	{
+
+		private DiscordSocketClient _client;
+		private CommandService _commands;
+		private IServiceProvider _services;
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			_client = new DiscordSocketClient();
+
+			_commands = new CommandService();
+
+			_services = new ServiceCollection()
+				.AddSingleton(_client)
+				.AddSingleton(_commands)
+				.BuildServiceProvider();
+
+			_client.Log += Log;
+
+			//  You can assign your bot token to a string, and pass that in to connect.
+			//  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+			var token = "ODU0NjgwMTkwMjk0MDMyMzk0.YMnc9A.Y5PFiNN_v8-T0qLsMH4ZhqaHA88";
+
+			// Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+			// var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+			// var token = File.ReadAllText("token.txt");
+			// var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+			await RegisterCommandsAsync();
+
+			await _client.LoginAsync(TokenType.Bot, token);
+			await _client.StartAsync();
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		public async Task RegisterCommandsAsync()
+		{
+			_client.MessageReceived += HandleCommandAsync;
+			await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+		}
+
+		private async Task HandleCommandAsync(SocketMessage arg)
+		{
+			var message = arg as SocketUserMessage;
+			var context = new SocketCommandContext(_client, message);
+			if (message.Author.IsBot) return;
+
+			int argPos = 0;
+			if (message.HasStringPrefix("~", ref argPos))
+			{
+				var result = await _commands.ExecuteAsync(context, argPos, _services);
+				Console.WriteLine(message);
+				Console.WriteLine(result);
+				if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+				if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+			}
+		}
+	}
+}
